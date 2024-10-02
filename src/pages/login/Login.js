5c16afb947f20c348ba5b6f9aee201beb9b4151f
fardhanrasya <fardhanhafist14@gmail.com>
2024-10-02 15:37:28 +0700

    updated login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,7 @@
 import './Login.css'
 import { useState } from 'react'
 import { useLogin } from '../../hooks/UseLogin'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
   const [email, setemail] = useState('')
@@ -22,6 +23,7 @@ export default function Login() {
         <span>Password: </span>
         <input type="password" onChange={e => setpassword(e.target.value)} value={password} required />
       </label>
+      <span>Don't have an account yet? </span> <Link to='/Signup'>Signup</Link> <br></br>
       {!isPending && <button className="btn">Login</button>}
       {isPending && <button disabled className="btn">Loading</button>}
       {error && <div className='error'>{error}</div>}
